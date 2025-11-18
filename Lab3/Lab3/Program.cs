1fd647a41288d5b17ec72126b1fc08a9735249e0
Khaled Jalal Al-arashi <77218749+k-j-alarashi@users.noreply.github.com>
2025-11-19 00:37:29 +0300

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP
+{
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+            Rectangle b = new Rectangle(5, 10, 20, 30);
+            b.display();
+            Console.WriteLine("Area = "+b.Area());
+            b.x = 100;
+            b.display();
+        }
+    }
+}
---
+++ Lab3/Lab3/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿public class Rectangle
+{
+    // fields
+    public int x;
+    public int y;
+    public double width;
+    public double height;
+
+    // constructor
+    public Rectangle(int x , int y , double width , double height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width =  width;
+        this.height = height;
+    }
+
+    //methods
+    public double Area()
+    {
+        return width * height;
+    }
+
+    public void display()
+    {
+        Console.WriteLine("============");
+        Console.WriteLine("x        = "+x);
+        Console.WriteLine("y        = "+y);
+        Console.WriteLine("width    = "+width);
+        Console.WriteLine("height   = "+height);
+        Console.WriteLine("============");
+    }
+}
